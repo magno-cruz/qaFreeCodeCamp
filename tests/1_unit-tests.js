de29e50d441dc200bd06d929f0a8b74629efd145
Magno <magnocruz@proton.me>
2023-02-19 22:52:03 -0300

    Teste 06.02

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -41,7 +41,7 @@ suite('Unit Tests', function () {
     // #6
     test('#strictEqual, #notStrictEqual', function () {
       assert.notStrictEqual(6, '6');
-      assert.notStrictEqual(6, 3 * 2);
+      assert.strictEqual(6, 3 * 2);
       assert.strictEqual(6 * '2', 12);
       assert.notStrictEqual([1, 'a', {}], [1, 'a', {}]);
     });
